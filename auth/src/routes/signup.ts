46404f3961805387d34bb63f9c6ce655bd429786
falco9017 <francesco.falcone90@gmail.com>
2020-06-07 22:14:22 +0200

    implemented signup with express validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('api/users/signup', (req, res) => {
-  res.send('Hi there');
-});
+//apply a validator as middleware to check if the res is correct
+router.post(
+  '/api/users/signup',
+  //adding a middleware as second argument
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  //typescript type validation
+  (req: Request, res: Response) => {
+    //validationResults return an object with specified messages
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    const { email, password } = req.body;
+    console.log('Creating a user');
+
+    res.send({ ciao: 'ciao' });
+  }
+);
 
 export { router as signupRouter };
