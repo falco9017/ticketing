0b9b7be894bcb2a76ebc48be57513c8bda99b365
falco9017 <francesco.falcone90@gmail.com>
2020-06-08 22:20:25 +0200

    added mongoose and db init

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 //this module handles async for express function
 import 'express-async-errors';
 import { json } from 'body-parser';
+
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -26,6 +29,29 @@ app.use(errorHandler);
 //   res.send('Hi');
 // });
 
-app.listen(3000, () => {
-  console.log('Listening on 3000');
-});
+//name of the db. If it does not exist mongo is going to create it
+const dbName = 'auth';
+//name of the service comes from auth-mongo-depl file
+const url = `mongodb://auth-mongo-srv:27017/${dbName}`;
+
+//starting function
+//all await async has to be wrapped for older node version
+const start = async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to mongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+
+  //start to listen for incoming traffic
+  app.listen(3000, () => {
+    console.log('Listening on 3000');
+  });
+};
+
+start();
