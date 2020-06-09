2a3403615ced9724f24958e9374aa41663281b3c
falco9017 <francesco.falcone90@gmail.com>
2020-06-09 10:29:51 +0200

    created bad request error and added to signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -17,16 +18,25 @@ router.post(
       .withMessage('Password must be between 4 and 20 characters'),
   ],
   //typescript type validation
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     //validationResults return an object with specified messages
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
+    //take email and pw from body
     const { email, password } = req.body;
-    console.log('Creating a user');
-    throw new DatabaseConnectionError();
-    res.send({ ciao: 'ciao' });
+
+    //check if the same email exists in the db already
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError('Email already in use');
+    }
+
+    //if not, create a new user and save it to db
+    const user = User.build({ email, password });
+    await user.save();
+    res.status(201).send(user);
   }
 );
 
