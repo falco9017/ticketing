4e6cf17788eef708d0cd8590523f85b84ef4403c
falco9017 <francesco.falcone90@gmail.com>
2020-06-10 09:53:51 +0200

    added toJSON method

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -20,16 +20,29 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  //this object is to customize json output of user model
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 //middleware function for operations to do before save
 //we can't use arrow
