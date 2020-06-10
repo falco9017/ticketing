942fcbf78408e34b47df533774ceb86159916c2f
falco9017 <francesco.falcone90@gmail.com>
2020-06-10 18:39:02 +0200

    refactored index.ts into two files

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,51 +1,11 @@
-import express from 'express';
-//this module handles async for express function
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
+import { app } from './app';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-//this is to work with nginx proxy and https
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    //we are not encrypting cookie because we send JWT
-    //which is already encrypted
-    signed: false,
-    //https connection
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-//if ruote does not exist throw an error
-app.get('*', () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
-
-// app.get('/api/users/test', (req, res) => {
-//   res.send('Hi');
-// });
-
+/////STARTING FUNCTION
 //name of the db. If it does not exist mongo is going to create it
 const dbName = 'auth';
 //name of the service comes from auth-mongo-depl file
 const url = `mongodb://auth-mongo-srv:27017/${dbName}`;
-
-//starting function
 //all await async has to be wrapped for older node version
 const start = async () => {
   //first check if env variable exists befor continuing
