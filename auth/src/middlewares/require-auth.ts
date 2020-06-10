5af990fdb2a139e6368dea2e4aa4e15fd1778eb0
falco9017 <francesco.falcone90@gmail.com>
2020-06-10 17:49:49 +0200

    created require auth middleware and error

---
+++ auth/src/middlewares/require-auth.ts
@@ -0,0 +1,15 @@
+//this middleware throw an error if the user is not logged in
+//it HAS to be called after current-user middleware
+import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError } from '../errors/not-authorized-error';
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.currentUser) {
+    throw new NotAuthorizedError();
+  }
+  next();
+};
