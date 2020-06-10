3c1087fb0562b188a8e9e636fbfefec99ebca007
falco9017 <francesco.falcone90@gmail.com>
2020-06-10 10:21:21 +0200

    added validate request middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there');
-});
+router.post(
+  '/api/users/signin',
+  //express-validator validation
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    //take email and pw from body
+    const { email, password } = req.body;
+  }
+);
 
 export { router as signinRouter };
