dfeb53057134c048fb0ec59fc0b76573a5877677
falco9017 <francesco.falcone90@gmail.com>
2020-06-11 15:24:55 +0200

    add bootstrap

---
+++ client/pages/_app.js
@@ -0,0 +1,7 @@
+//this file adds styling to all our pages
+//the file redefines the app component
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
