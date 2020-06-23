d8eb19775209db5d2d2f488214dec2ee487bed11
falco9017 <francesco.falcone90@gmail.com>
2020-06-23 09:25:18 +0200

    modified _app to run getInitProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,35 @@
 //this file adds styling to all our pages
 //the file redefines the app component
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+//don't call it App
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  console.log(currentUser);
+  return (
+    <div>
+      <h1>Hello {}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+//in the _app file this function receives a different props
+//the req, res are inside ctx property of the context
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  //getInitialProps in other pages is not called since we implemented this
+  //so we need to call it manually
+  console.log(data);
+  const pageProps = await appContext.Component.getInitialProps?.(
+    appContext.ctx
+  );
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
