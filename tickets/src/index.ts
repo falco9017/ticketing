a3762e63879b9e49c4f9577595013c70f6f511d9
falco9017 <francesco.falcone90@gmail.com>
2020-09-07 12:16:52 +0200

    added env variables for nats connection

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,12 +13,25 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
 
   //connect to mongodb
   try {
     //clusterId is taken from nats-depl.yaml
     //url is taken from service name and port in yaml file
-    await natsWrapper.connect('ticketing', 'fueveiu', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     //handle closing of NATS
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed');
